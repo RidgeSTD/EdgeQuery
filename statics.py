3d491f162e40b2292c6a7a0638077a26c11bcf5c
Winlandiano <winlandiano@gmail.com>
2016-01-07 16:20:51 +0800

    添加大图上一个测试结果

--- statics.py
+++ statics.py
@@ -2,12 +2,15 @@ ans_io_time = 0.0
 
 f_cons = None
 
-neighbor_threshold = 3
-
-fade_factor = 0.5
 
 fade_factor_pow = []
 
 io_tree_node_count = 0
+
+# ====== factors =======
+
+neighbor_threshold = 3
+
+fade_factor = 0.5
 
 answer_k = 20
--- testfile.py
+++ testfile.py
@@ -5,52 +5,7 @@ import common
 
 
 def main():
-    b = [1, 2, 3, 4, 8, 9, 10, 11]
-    c = [1, 2, 3, 4, 8, 9, 10, 11]
-    a = [7, 100, 101, 102]
-    d = [7, 100, 101, 102]
-    sa = set(a)
-    sb = set(b)
-    aa = 0
-    print('===========循环访问=============')
-    t1 = clock()
-    for i in range(1, 10000):
-        for j in range(0, len(a)):
-            t = a[j]
-        for j in range(0, len(b)):
-            t = b[j]
-        aa = t
-    print(clock() - t1)
-    print('===========extend=============')
-    t1 = clock()
-    for i in range(1, 10000):
-        c.extend(d)
-        c = [1, 2, 3, 4, 8, 9, 10, 11]
-    print(clock() - t1)
-    print('==========我并系统并==============')
-    t1 = clock()
-    for i in range(1, 10000):
-        common.union(a, b)
-    print(clock() - t1)
 
-    t1 = clock()
-    for i in range(1, 10000):
-        sa = set(a)
-        sb = set(b)
-        sa.union(sb)
-    print(clock() - t1)
-    print('========我交系统交================')
-    t1 = clock()
-    for i in range(1, 10000):
-        common.intersect(a, b)
-    print(clock() - t1)
-
-    t1 = clock()
-    for i in range(1, 10000):
-        sa = set(a)
-        sb = set(b)
-        sa.intersection(sb)
-    print(clock() - t1)
 
 
 if __name__ == "__main__":
